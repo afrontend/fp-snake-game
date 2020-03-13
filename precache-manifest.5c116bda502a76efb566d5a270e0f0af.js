1356592a93269f6a86e15ba5cbf538aff7648c56
afrontend <autoscripts@gmail.com>
2020-03-13 23:12:16 +0900

    Updates

---
+++ precache-manifest.5c116bda502a76efb566d5a270e0f0af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "718e02ed666c48556226",
+    "url": "/fp-snake-game/static/js/runtime~main.718e02ed.js"
+  },
+  {
+    "revision": "eb32e9a937d40a33d1ce",
+    "url": "/fp-snake-game/static/js/main.eb32e9a9.chunk.js"
+  },
+  {
+    "revision": "48afd4e237b224629279",
+    "url": "/fp-snake-game/static/js/1.48afd4e2.chunk.js"
+  },
+  {
+    "revision": "eb32e9a937d40a33d1ce",
+    "url": "/fp-snake-game/static/css/main.98ac3b40.chunk.css"
+  },
+  {
+    "revision": "1753567a083556f99599952e9ee4e4ce",
+    "url": "/fp-snake-game/index.html"
+  }
+];
