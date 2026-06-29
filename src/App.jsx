b48215661cedcf6b81e808be5b1486c0a5821987
afrontend <autoscripts@gmail.com>
2026-06-29 17:32:47 +0900

    fix: GitHub 아이콘을 게임 화면 기준 우상단으로 이동
    뷰포트 전체 기준 fixed 위치에서 게임 보드(.App) 기준 absolute 위치로 변경
    .App에 position: relative 추가

--- src/App.jsx
+++ src/App.jsx
@@ -94,6 +94,9 @@ function App() {
     : (
       <div className="container">
         <div className="App">
+          <a href="https://github.com/afrontend/fp-snake-game" title="fp-snake-game" style={{ position: 'absolute', top: 8, right: 8, zIndex: 100 }}>
+            <img style={{ width: 20, height: 20 }} src="https://agvim.files.wordpress.com/2015/08/github-mark-32px.png?w=685" alt="GitHub" />
+          </a>
           {showHelp ? (
             <div className="help-overlay" role="dialog" aria-label="도움말">
               <table>
